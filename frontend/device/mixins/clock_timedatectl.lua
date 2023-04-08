8d31cbf1c80beddd9270972195a0a5854d8798bb
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-04-08 15:46:06 -0400

    mixins/clock_timedatectl: Detect automatic time sync
    It's not possible (and not correct) to set the time manually in these
    instances.

--- frontend/device/mixins/clock_timedatectl.lua
+++ frontend/device/mixins/clock_timedatectl.lua
@@ -1,6 +1,15 @@
+local logger = require("logger")
+
 return function(class)
 	class.setDateTime = function(self, year, month, day, hour, min, sec)
-		if hour == nil or min == nil then return true end
+		if hour == nil or min == nil then
+			-- Detect if we can change the time
+			result = os.execute("timedatectl show | grep ^NTP=yes &> /dev/null") ~= 0
+			if not result then
+				logger.info("mixins/clock_timedatectl: Automatic time synchronization is enabled; clock backend disabled")
+			end
+			return result
+		end
 		local command
 		if year and month and day then
 			command = string.format("timedatectl set-time '%d-%d-%d %d:%d:%d'", year, month, day, hour, min, sec)
