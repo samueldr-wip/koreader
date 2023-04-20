07c2ef222bea3631e45d0212f113c05af256522d
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-04-20 15:44:44 -0400

    device/sdl: Extract Emulator in its own file
    This aims to reduce complexity in the main SDLDevice description.

--- frontend/device/sdl/device.lua
+++ frontend/device/sdl/device.lua
@@ -119,23 +119,6 @@ local Desktop = SDLDevice:extend{
     hasExitOptions = notOSX,
 }
 
-local Emulator = SDLDevice:extend{
-    model = "Emulator",
-    isEmulator = yes,
-    hasBattery = yes,
-    hasEinkScreen = yes,
-    hasFrontlight = yes,
-    hasNaturalLight = yes,
-    hasNaturalLightApi = yes,
-    hasWifiToggle = yes,
-    -- Not really, SDLDevice:reboot & SDLDevice:powerOff are not implemented, so we just exit ;).
-    canPowerOff = yes,
-    canReboot = yes,
-    -- NOTE: Via simulateSuspend
-    canSuspend = yes,
-    canStandby = no,
-}
-
 local UbuntuTouch = SDLDevice:extend{
     model = "UbuntuTouch",
     hasFrontlight = yes,
@@ -378,42 +361,7 @@ function SDLDevice:initNetworkManager(NetworkMgr)
     end
 end
 
-function Emulator:supportsScreensaver() return true end
-
-function Emulator:simulateSuspend()
-    local Screensaver = require("ui/screensaver")
-    Screensaver:setup()
-    Screensaver:show()
-end
-
-function Emulator:simulateResume()
-    local Screensaver = require("ui/screensaver")
-    Screensaver:close()
-end
-
--- fake network manager for the emulator
-function Emulator:initNetworkManager(NetworkMgr)
-    local UIManager = require("ui/uimanager")
-    local connectionChangedEvent = function()
-        if G_reader_settings:nilOrTrue("emulator_fake_wifi_connected") then
-            UIManager:broadcastEvent(Event:new("NetworkConnected"))
-        else
-            UIManager:broadcastEvent(Event:new("NetworkDisconnected"))
-        end
-    end
-    function NetworkMgr:turnOffWifi(complete_callback)
-        G_reader_settings:flipNilOrTrue("emulator_fake_wifi_connected")
-        UIManager:scheduleIn(2, connectionChangedEvent)
-    end
-    function NetworkMgr:turnOnWifi(complete_callback)
-        G_reader_settings:flipNilOrTrue("emulator_fake_wifi_connected")
-        UIManager:scheduleIn(2, connectionChangedEvent)
-    end
-    function NetworkMgr:isWifiOn()
-        return G_reader_settings:nilOrTrue("emulator_fake_wifi_connected")
-    end
-    NetworkMgr.isConnected = NetworkMgr.isWifiOn
-end
+local Emulator = require("device/sdl/emulator")(SDLDevice)
 
 logger.info("Starting SDL in:", SDL.getBasePath())
 
