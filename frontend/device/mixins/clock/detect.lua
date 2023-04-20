6500303ead02dfd0eb7c1784085b2be6bc83287e
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-04-20 17:41:22 -0400

    device/mixins: Add auto-detect clock backend
    This is intended to be used on generic devices.

---
+++ frontend/device/mixins/clock/detect.lua
@@ -0,0 +1,19 @@
+local logger = require("logger")
+local clock_timedatectl = require("device/mixins/clock/timedatectl")
+local clock_hwclock = require("device/mixins/clock/hwclock")
+local clock_none = function() end
+
+return function(class)
+		logger.info("mixins/clock/detect: Detecting clock backend...")
+		if os.execute("timedatectl show &>/dev/null") == 0 then
+			logger.info("  -> timedatectl")
+			return clock_timedatectl(class)
+		elseif os.execute("hwclock -r &>/dev/null") == 0 then
+			logger.info("  -> hwclock")
+			return clock_hwclock(class)
+		else
+			logger.info("  -> none")
+			return clock_none(class)
+		end
+end
+
