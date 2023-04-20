ea5c383f9808f57974b2aecc27628ce09604ebc5
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-04-20 17:42:52 -0400

    device/sdl: Use auto-detect clock backend

--- frontend/device/sdl/device.lua
+++ frontend/device/sdl/device.lua
@@ -272,21 +272,7 @@ function SDLDevice:init()
     Generic.init(self)
 end
 
-function SDLDevice:setDateTime(year, month, day, hour, min, sec)
-    if hour == nil or min == nil then return true end
-    local command
-    if year and month and day then
-        command = string.format("date -s '%d-%d-%d %d:%d:%d'", year, month, day, hour, min, sec)
-    else
-        command = string.format("date -s '%d:%d'",hour, min)
-    end
-    if os.execute(command) == 0 then
-        os.execute('hwclock -u -w')
-        return true
-    else
-        return false
-    end
-end
+require("device/mixins/clock/detect")(SDLDevice)
 
 -- Guess at the network backend to use
 require("device/mixins/network/detect")(SDLDevice)
